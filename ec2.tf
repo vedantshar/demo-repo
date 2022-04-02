495c485df6b0dc69bee35986d37279a01f910848
vedantshar <37017870+vedantshar@users.noreply.github.com>
2022-04-02 17:22:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region = "us-west-2"
-  access_key = "AKIA6AQ4WNNWV6QO4OS6"
-  secret_key = "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"
-}
-
 resource "aws_instance" "my_ec2"{
   ami = "ami-00ee4df451840fa9d"
   instance_type = "t2.micro"
